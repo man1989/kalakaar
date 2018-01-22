59373181123f0892b6a70787a3fed63f0f5fe066
Kumar <kumarma@hyd-kumarma-m.ds.ad.adp.com>
2018-01-22 21:22:03 +0530

    honour PORT

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 module.exports = function(){
     return {
-        PORT: process.env.APP_PORT || 8080,
+        PORT: process.env.APP_PORT||process.env.PORT|| 8080,
         MONGODB_URI: process.env.MONGODB_URI || "mongodb://localhost/kalakaar"
     }
 }
